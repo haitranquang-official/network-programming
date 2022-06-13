ee60d6878c99e44667387575101c37fa50c6adb6
Hai Tran Quang <75372438+haitranquang-official@users.noreply.github.com>
2022-06-13 07:43:42 +0700

    Initialize database

---
+++ database/initDB.c
@@ -0,0 +1,40 @@
+#include <mysql/mysql.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    MYSQL* connection = mysql_init(NULL);
+
+    if (connection == NULL) {
+        printf("%s\n", mysql_error(connection));
+        exit(1);
+    }
+
+    if (mysql_real_connect(connection,
+    "localhost", "BUDA", "BUDA", NULL, 0, NULL, 0) == NULL) {
+        printf("Invalid credentials");
+        mysql_close(connection);
+        exit(1);
+    } 
+
+    mysql_query(connection, "CREATE DATABASE network_programming");
+
+    mysql_query(connection, "USE network_programming");
+
+    mysql_query(connection,
+    "DROP TABLE IF EXISTS user");
+
+    mysql_query(connection, 
+    "CREATE TABLE user(id INT PRIMARY KEY AUTO_INCREMENT,"
+    " username VARCHAR(255) UNIQUE,"
+    " password VARCHAR(255))");
+
+    mysql_query(connection,
+    "DROP TABLE IF EXISTS resource");
+
+    mysql_query(connection,
+    "CREATE TABLE resource(id INT PRIMARY KEY AUTO_INCREMENT,"
+    " user_id INT NOT NULL,"
+    " FOREIGN KEY (user_id) REFERENCES user(id))");
+}
