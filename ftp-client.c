9aeda300494bf760f85045ea87a49c16c6fed7a5
Hieu Tran <hieu.trantrung2908@gmail.com>
2022-07-04 20:10:17 +0700

    tách login thành 1 hàm

--- ftp-client.c
+++ ftp-client.c
@@ -11,6 +11,9 @@
 #include <pthread.h>
 #include <dirent.h>
 
+int sfd = -1;
+int dfd = -1;
+
 void append(char* data, char** presp)
 {
     int oldlen = (*presp == NULL ? 0 : strlen(*presp));
@@ -19,27 +22,31 @@ void append(char* data, char** presp)
     sprintf(*presp + strlen(*presp), "%s", data);
 }
 
-int main(int argc, char** argv)
-{
-    char userName[1024];
-    char password[1024];
-    char *buffer = NULL;
+void signal_handler(int sig) {
+    close(sfd);
+    close(dfd);
+    exit(0); //Terminate the main process
+}
 
-    memset(userName, 0 , sizeof(userName));
+void connect_to_server() {
+    char username[256];
+    char password[256];
+    char buffer[1024];
+
+    memset(username, 0 , sizeof(username));
     memset(password, 0 , sizeof(password));
+    memset(buffer, 0, sizeof(buffer));
 
     printf("Input username: \n");
-    scanf("%s", userName);
+    scanf("%s", username);
 
     printf("Input password: \n");
     scanf("%s", password);
 
-    append("userName: ", &buffer);
-    append(userName, &buffer);
-    append(" password: ", &buffer);
-    append(password, &buffer);
+    sprintf(buffer, "LOGIN %s %s", username, password);
 
-    int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    // đây mới là kết nối tới command port
+    sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sfd >= 0) {
         struct sockaddr_in saddr;
         saddr.sin_family = AF_INET;
@@ -49,18 +56,45 @@ int main(int argc, char** argv)
         int result = connect(sfd, (struct sockaddr*)&saddr, sizeof(saddr));
 
         if(result == 0) {
-            while(1) {
-                send(sfd, buffer, strlen(buffer), 0);
+            // nhận welcome message từ server
+            char welcome_message[1024];
+            memset(welcome_message, 0, sizeof(welcome_message));
+            recv(sfd, welcome_message, sizeof(welcome_message), 0);
+            printf("%s", welcome_message);
 
-                char reply[1024*20];
-                memset(reply, 0, sizeof(reply));            
+            // gửi yêu cầu LOGIN lên server
+            send(sfd, buffer, strlen(buffer), 0);
 
-                result = recv(sfd, reply, sizeof(reply), 0);
-                if(result < 0) {
-                    break;
-                }
-            }
+            memset(buffer, 0, sizeof(buffer));
+            recv(sfd, buffer, sizeof(buffer), 0);
+            printf("%s", buffer);
+
+            // gọi lệnh PASV để thiết lập data port theo kiểu passive
         }
-        close(sfd);
     }
+}
+
+int main(int argc, char** argv)
+{
+
+    connect_to_server();
+
+    // char buffer[1024];
+    // memset(buffer, 0, sizeof(buffer));
+
+    // while(1) {
+    //     send(sfd, buffer, strlen(buffer), 0);
+
+    //     char reply[1024*20];
+    //     memset(reply, 0, sizeof(reply));            
+
+    //     int result = recv(sfd, reply, sizeof(reply), 0);
+    //     if(result < 0) {
+    //         break;
+    //     }
+    // }
+
+    close(sfd);
+
+    return 0;
 }
