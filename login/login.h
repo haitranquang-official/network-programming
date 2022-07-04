13acf8da871012d5bb522cfc64dfab690ea3a486
Hieu Tran <hieu.trantrung2908@gmail.com>
2022-07-04 09:47:41 +0700

    added login feature

---
+++ login/login.h
@@ -0,0 +1,6 @@
+/**
+ * Authenticate user based on username and password.
+ * Return user_id of the authenticated user if successful,
+ * return -1 otherwise
+*/
+int login(char username[256], char password[256]);
